ff01f462b2673b02c92d6c23086762d52e699c10
Evan Guirino <etg624@gmail.com>
2018-12-21 09:21:57 -0800

    added POST testing

--- test/test-recipe.js
+++ test/test-recipe.js
@@ -1,3 +1,4 @@
+'use strict'
 const chai = require("chai");
 const chaiHttp = require("chai-http");
 
@@ -13,13 +14,13 @@ const expect = chai.expect;
 // see: https://github.com/chaijs/chai-http
 chai.use(chaiHttp);
 
-describe("Recipe List", function() {
+describe("Recipe List", function () {
   // Before our tests run, we activate the server. Our `runServer`
   // function returns a promise, and we return the that promise by
   // doing `return runServer`. If we didn't return a promise here,
   // there's a possibility of a race condition where our tests start
   // running before our server has started.
-  before(function() {
+  before(function () {
     return runServer();
   });
 
@@ -28,7 +29,7 @@ describe("Recipe List", function() {
   // if we add another test module that also has a `before` block
   // that starts our server, it will cause an error because the
   // server would still be running from the previous tests.
-  after(function() {
+  after(function () {
     return closeServer();
   });
 
@@ -50,4 +51,23 @@ describe("Recipe List", function() {
         });
       });
   });
+
+  it('should add a recipe on POST', function () {
+    const newItem = { name: 'bread', ingredients: ['flour', 'eggs', 'water'] };
+    return chai
+      .request(app)
+      .post('/recipes')
+      .send(newItem)
+      .then(function (res) {
+        expect(res).to.have.status(201);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('object');
+        expect(res.body).to.include.keys('id', 'name', 'ingredients');
+        expect(res.body.id).to.not.eq(null);
+        expect(res.body).to.deep.eq(Object.assign(newItem, { id: res.body.id }));
+
+      });
+
+  });
+
 });
